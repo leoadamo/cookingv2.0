af56b4103dfcb036bbc4ebe6c6a01f0a15560a56
Leonardo Adamoli <leo.adamoli@gmail.com>
2019-09-24 10:41:35 -0300

    instalacao de pacotes e configuracao do gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,33 @@
-const { src, dest, paralell } = require('gulp')
+const { src, dest, watch, parallel } = require('gulp')
 const sass = require('gulp-sass')
+const bs = require('browser-sync').create()
+
+function browserSync(done) {
+  bs.init({
+    server: {
+      baseDir: 'app/'
+    },
+    port: 3000
+  })
+  done()
+}
+
+function bsReload(done) {
+  bs.reload()
+  done()
+}
 
 function css() {
-  return src('app/scss/main.scss')
-    .pipe(sass())
+  return src('app/scss/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(dest('app/css'))
+    .pipe(bs.stream())
+}
+
+function watchFiles() {
+  watch('app/scss/**/*.scss', css)
+  watch('app/*.html', bsReload)
 }
 
 exports.css = css
+exports.watch = parallel(watchFiles, browserSync)
