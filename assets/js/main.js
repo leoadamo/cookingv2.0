5692959e6aa96a52d3197dcee1ab4d52418f0e84
Leonardo Adamoli <leo.adamoli@gmail.com>
2019-10-30 10:31:01 -0300

    ajustes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
-// VENDOR
-import './vendor/jquery';
+import jquery from 'jquery';
 import 'jquery-validation';
 import 'jquery-validation/dist/additional-methods';
 
 // COMPONENTS
 import toggleMenu from './components/toggle-menu';
 import formValidate from './components/form-validate';
+
+window.$ = jquery;
 
 // FUNCTIONS
 $(() => {
