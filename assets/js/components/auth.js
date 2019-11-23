ce28a430f677f911a4c18923638c024da74a2d9b
Leonardo Adamoli <leo.adamoli@gmail.com>
2019-11-23 17:05:50 -0300

    implementando redirect para a pagina de intro ao identificar que o usuario esta logado

--- assets/js/components/auth.js
+++ assets/js/components/auth.js
@@ -4,16 +4,23 @@ export default () => {
 			Auth.bind.call();
 		},
 		cache: {
-			spinner: $(".js-loader-trigger")
+			spinner: $(".js-loader-trigger"),
+			logout: $(".js-logout-trigger")
 		},
 		bind: () => {
 			Auth.functions.authenticate();
+			Auth.cache.logout.on("click", Auth.functions.deleteCookie);
 		},
 		functions: {
 			authenticate: () => {
 				if (!Auth.functions.getCookie("login")) {
-					if (window.location.pathname === "/feed.html" || window.location.pathname === "/contato.html") {
+					if (window.location.pathname === "/feed.html" || window.location.pathname === "/contato.html" || window.location.pathname === "/intro.html") {
 						window.location.replace("/login.html");
+						return;
+					}
+				} else {
+					if (window.location.pathname === "/login.html") {
+						window.location.replace("/intro.html");
 						return;
 					}
 				}
@@ -22,6 +29,15 @@ export default () => {
 			getCookie: name => {
 				const v = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
 				return v ? v[2] : null;
+			},
+			setCookie: (name, value, days) => {
+				const d = new Date();
+				d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
+				document.cookie = `${name} = ${value} ;path=/;expires= ${d.toGMTString()}`;
+			},
+			deleteCookie: () => {
+				Auth.functions.setCookie("login", "", -1);
+				window.location.reload();
 			},
 			toggleSpinner: () => {
 				Auth.cache.spinner.toggleClass("hidden");
--- assets/js/components/login.js
+++ assets/js/components/login.js
@@ -78,9 +78,6 @@ export default () => {
 				const d = new Date();
 				d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
 				document.cookie = `${name} = ${value} ;path=/;expires= ${d.toGMTString()}`;
-			},
-			deleteCookie: name => {
-				Login.functions.setCookie(name, "", -1);
 			}
 		}
 	};
